e181c06b9a648f93dcd19997bac7c9ada12871be
sebygalla <sebygalla@hotmail.com>
2024-10-14 00:18:18 -0300

    creacion de componente boton y paso de props a dicho boton en app.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,14 +1,17 @@
 import React from "react";
 import Card from "./components/Card";
+import Button from "./components/button";
 
 function App() {
   return (
-    <div className="contenedor">
-       
-        <Card/>
-      
-      
+    <><div className="contenedor">
+      <Card />
     </div>
+
+    <div className="contenedorDos">
+        <Button text= "Random" />
+        <Button text= "Click" />
+    </div></>
   );
 }
 
