34de836a62079d2710437990d60e51257ea28949
sebygalla <sebygalla@hotmail.com>
2024-10-18 19:48:12 -0300

    cree la card y ordene input y button

---
+++ frontend/src/components/card.js
@@ -0,0 +1,21 @@
+
+const Card = () => {
+    return (
+       
+        <div className="card">
+          <div className="card-overlay" />
+          <div className="card-inner">
+            MI 
+            <br />
+            CONTENIDO
+            <br />
+            AQUI
+          </div>
+        </div>
+
+    )
+
+    
+};
+
+export default Card;
