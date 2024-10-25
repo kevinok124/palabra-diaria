2165266c9b836242e657d8166d33fd02910a4ce7
sebygalla <sebygalla@hotmail.com>
2024-10-25 01:24:29 -0300

    agregamos la logica del componente random

--- frontend/src/components/randomItemPicker.js
+++ frontend/src/components/randomItemPicker.js
@@ -1,36 +1,72 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from "react";
+import myData from "../Schema.json";
 
+// const jsonData = [
+//   {Index:10},
+//   {Index:20},
+//   {Index:30},
+//   {Index:40},
+// ];
 
-
-
-function RandomItemPicker({ data }) {
+const RandomItemPicker = () => {
   const [randomItem, setRandomItem] = useState(null);
 
-  const getRandomItem = () => {
-    const randomIndex = Math.floor(Math.random() * data.length);
-    setRandomItem(data[randomIndex]);
+  const seleccionarElementoAleatorio = () => {
+    let nuevoElemento;
+    do {
+      const randomIndex = Math.floor(Math.random() * myData.length);
+      nuevoElemento = myData[randomIndex];
+    } while (nuevoElemento === randomItem);
+    setRandomItem(nuevoElemento);
   };
+
+  useEffect(() => {
+    seleccionarElementoAleatorio();
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
 
   return (
     <div>
-      <button onClick={getRandomItem}>Obtener elemento aleatorio</button>
+      <button onClick={seleccionarElementoAleatorio}>
+        Obtener elemento aleatorio
+      </button>
       {randomItem && (
         <div>
-        <div className="card">
-          <div className="card-overlay" />
-          <div className="card-inner">
-            {/* Aquí puedes mostrar las propiedades del elemento aleatorio */}
-             <p>{randomItem.Book}</p>
-             <p>{randomItem.Testament}</p>
-             <p>{randomItem.Verse}</p>
-             <p>{randomItem.Text}</p>
-            {/* ... y así sucesivamente */}            
-          </div>
+          <div className="card">
+            <div className="card-overlay" />
+            <div className="card-inner">
+              {/* Aquí puedes mostrar las propiedades del elemento aleatorio */}
+              <p>
+                <strong>numero: </strong>
+                {randomItem.Index}
+              </p>
+              <p>
+                <strong>testamento: </strong>
+                {randomItem.Testament}
+              </p>
+              <p>
+                <strong>libro: </strong>
+                {randomItem.Book}
+              </p>
+              <p>
+                <strong>numero de libro: </strong>
+                {randomItem.BoookNumber}
+              </p>
+              <p>
+                <strong>versiculo: </strong>
+                {randomItem.Verse}
+              </p>{" "}
+              <p>
+                <strong>palabra diaria: </strong>
+                {randomItem.Text}
+              </p>
+              {/* ... y así sucesivamente */}
+            </div>
           </div>
         </div>
       )}
     </div>
   );
-}
+};
 
 export default RandomItemPicker;
