9515d0d73f505df84c8aa6019e9f4424d4c972e2
Kevin Okulczyk <kevin.okulczyk@gmail.com>
2024-10-30 17:29:34 -0300

    actualización de randonItemPicker y unificación de ramas

--- frontend/src/components/randomItemPicker.js
+++ frontend/src/components/randomItemPicker.js
@@ -1,82 +1,74 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useCallback } from "react";
 
 const RandomItemPicker = () => {
-  const [randomItem, setRandomItem] = useState(null);
-  const [data, setData] = useState([]);
+    const [randomItem, setRandomItem] = useState(null);
+    const [data, setData] = useState([]);
 
-  useEffect(() => {
-    // Realizar la solicitud al backend para obtener los datos
-    const fetchData = async () => {
-      try {
-        const response = await fetch("http://localhost:5000/api/versiculos");
-        const result = await response.json();
-        setData(result);
-        if (result.length > 0) {
-          seleccionarElementoAleatorio(result);
-        }
-      } catch (error) {
-        console.error("Error al obtener los datos:", error);
-      }
-    };
+    // Función para seleccionar un elemento aleatorio
+    const seleccionarElementoAleatorio = useCallback(() => {
+        if (data.length === 0) return; // Verificar que datos no esté vacío
+        let nuevoElemento;
+        do {
+            const randomIndex = Math.floor(Math.random() * data.length);
+            nuevoElemento = data[randomIndex];
+        } while (nuevoElemento === randomItem);
+        setRandomItem(nuevoElemento);
+    }, [data, randomItem]);
 
-    fetchData();
-  }, []);
+    useEffect(() => {
+        // Realizar la solicitud al backend para obtener los datos
+        const fetchData = async () => {
+            try {
+                const response = await fetch("http://localhost:5000/api/versiculos");
+                const result = await response.json();
+                setData(result);
+            } catch (error) {
+                console.error("Error al obtener los datos:", error);
+            }
+        };
 
-  const seleccionarElementoAleatorio = (datos = data) => {
-    if (datos.length === 0) return; // Verificar que datos no esté vacío
-    let nuevoElemento;
-    do {
-      const randomIndex = Math.floor(Math.random() * datos.length);
-      nuevoElemento = datos[randomIndex];
-    } while (nuevoElemento === randomItem);
-    setRandomItem(nuevoElemento);
-  };
+        fetchData();
+    }, []); // Solo se ejecuta una vez al montar el componente
 
-  return (
-    <div>
-      <button onClick={seleccionarElementoAleatorio}>
-        Obtener elemento aleatorio
-      </button>
-      {randomItem && (
+    return (
         <div>
-          <div className="card">
-       
-            <div className="card-inner">
-              <p>
-                <strong>Número: </strong>
-                {randomItem.Index}
-              </p>
-              <p>
-                <strong>Testamento: </strong>
-                {randomItem.Testament}
-              </p>
-              <p>
-                <strong>Libro: </strong>
-                {randomItem.Book}
-              </p>
-              <p>
-                <strong>Número de libro: </strong>
-                {randomItem.BoookNumber}
-              </p>
-              <p>
-                <strong>Versículo: </strong>
-                {randomItem.Verse}
-              </p>
-              <p>
-                <strong>Palabra diaria: </strong>
-                {randomItem.Text}
-              </p>
-            </div>
-          </div>
+            <button onClick={seleccionarElementoAleatorio}>
+                Obtener elemento aleatorio
+            </button>
+            {randomItem && (
+                <div>
+                    <div className="card">
+                        <div className="card-inner">
+                            <p>
+                                <strong>Número: </strong>
+                                {randomItem.Index}
+                            </p>
+                            <p>
+                                <strong>Testamento: </strong>
+                                {randomItem.Testament}
+                            </p>
+                            <p>
+                                <strong>Libro: </strong>
+                                {randomItem.Book}
+                            </p>
+                            <p>
+                                <strong>Número de libro: </strong>
+                                {randomItem.BookNumber}
+                            </p>
+                            <p>
+                                <strong>Versículo: </strong>
+                                {randomItem.Verse}
+                            </p>
+                            <p>
+                                <strong>Palabra diaria: </strong>
+                                {randomItem.Text}
+                            </p>
+                        </div>
+                    </div>
+                </div>
+            )}
         </div>
-      )}
-
-      <button onClick={seleccionarElementoAleatorio}>
-        Random
-      </button>
-      
-    </div>
-  );
+    );
 };
 
 export default RandomItemPicker;
